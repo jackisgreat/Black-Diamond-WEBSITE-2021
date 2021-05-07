e6c24cd0a06fcb8df1db6b71b14619c4f0e3edb8
jackisgreat <thejackfarrell@gmail.com>
2021-05-06 18:00:44 -0700

    navbar template

--- java.js
+++ java.js
@@ -1,1 +1,22 @@
-// JavaScript Document
+// JavaScript Document 
+
+
+//on page ready
+
+$( document ).ready(function() {
+    console.log( "ready!" );
+    
+    $(".blue").hover(function(){
+  $(this).css("background-color", "black");
+  }, function(){
+  $(this).css("background-color", "blue");
+});
+
+
+
+
+
+
+
+
+});
